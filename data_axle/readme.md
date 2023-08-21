b1497f4715ea7b7158f720926a512468fa89f01b
indragtzade <indragtzade@gmail.com>
2023-08-21 11:11:21 +0530

    added readme.md and requirements.txt files

---
+++ data_axle/readme.md
@@ -0,0 +1,10 @@
+Train Reservation System (TRS)
+
+Assumptions:
+1. Seats to be reserved in same cabin, it is assumed that seat number will be reserved in order
+    i.e. reserved = 5 seats means numbers for this will be [1 2 3 4 5]
+
+2. Initially all seats will be available.
+
+3. seat reserved = 5  so available seats will be [5 6 7 8 9 10] if capacity (fc_available or sc_available) is 10
+
--- data_axle/trs/class_config.py
+++ data_axle/trs/class_config.py
@@ -1,8 +1,22 @@
+
+# Number of cabins in First Class 
 fc_num_of_cabins = 2
+
+# Number of cabins in Second Class
 sc_num_of_cabins = 3
+
+# Number of Seats available in first class initially  total seats
 fc_available = 10
+
+# Number of Seats available in second class initially total seats
 sc_available = 15
+
+# Number of Seats reserved in first class
 fc_reversed = 4
+
+# Number of Seats reserved in second class
 sc_reserved = 5
+
+
 FIRST_CLASS_FARE = 400
 SECOND_CLASS_FARE = 300
